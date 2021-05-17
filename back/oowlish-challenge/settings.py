1e535834794ce7de98215d1d04cec0ec8e252299
Lucas <lucasmag97@gmail.com>
2021-05-17 18:03:13 -0300

    pass the envs settings to docker-compose.yml allow all hosts set database URL for each environment

--- back/oowlish-challenge/settings.py
+++ back/oowlish-challenge/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
 TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = ["localhost"]
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
--- back/start_server.sh
+++ back/start_server.sh
@@ -2,15 +2,12 @@
 # start_server.sh
 
 environment="$1"
-echo "$environment"
 
 if [ "$environment" = "production" ]; then
-  export ENV_PATH=".env.prod"
   database_host="db"
 else
   database_host="localhost"
 fi
-
 
 until PGPASSWORD=oowlishyay psql -h "$database_host" -U "oowlish" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
